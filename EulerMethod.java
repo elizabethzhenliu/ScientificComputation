6931c7230add9ee80b00b8d0ee90ca21d49b2600
elizabethzhenliu <elizabethzhenliu@gmail.com>
2015-06-09 22:41:12 -0700

    add EulerMethod.java

---
+++ EulerMethod.java
@@ -0,0 +1,39 @@
+
+public class EulerMethod {
+
+	public static double myDeriv(double function){
+		double derivative;
+		
+		//the function
+		derivative = -9.8 -.002*(Math.pow(function,2)/.11);
+		//derivative = .1*.02*(1-function);
+	
+		
+		return derivative;
+	}
+	
+	public static void euler(double y){
+		
+		double step = .1;
+		double n = 10;
+		for (int i = 0; i< n; i++){
+			y = y + step*myDeriv(y);
+			System.out.println(y);
+		}
+		
+		/*
+		double step = 1;
+		double n = 50;
+		for (int i = 0; i< n; i++){
+			y = y + step*myDeriv(y);
+			System.out.println(y);
+		}
+		*/
+		
+	}
+	
+	public static void main (String[] args){
+		//euler(.01);
+		euler(8);
+	}
+}
